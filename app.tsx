0722559085824556325324c3cd798d55f4b799a4
Tamoor Faisal <tamoorfaisal6@gmail.com>
2026-09-25 17:35:41 +1200

    fix: add navigation theme fonts; add tsconfig.json

--- app.tsx
+++ app.tsx
@@ -46,7 +46,7 @@ function Navigator() {
   }
 
   return (
-    <NavigationContainer
+     <NavigationContainer
       theme={{
         dark: true,
         colors: {
@@ -56,6 +56,12 @@ function Navigator() {
           text: colors.text,
           border: colors.border,
           notification: colors.gold,
+        },
+        fonts: {
+          regular: { fontFamily: 'Inter_400Regular', fontWeight: '400' },
+          medium: { fontFamily: 'Inter_500Medium', fontWeight: '500' },
+          bold: { fontFamily: 'Inter_700Bold', fontWeight: '700' },
+          heavy: { fontFamily: 'Inter_700Bold', fontWeight: '700' },
         },
       }}
     >
